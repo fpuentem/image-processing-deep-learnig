85a210159dce2372de66825f2b51821b723e64eb
Fab <fpuentem@uni.pe>
2021-05-17 16:41:09 -0300

    Fix West shift

--- week7/homework/hw-w7-v2.py
+++ week7/homework/hw-w7-v2.py
@@ -10,56 +10,61 @@ import time# the data, split between train and test sets
 # Load MNIST data
 (AX, ay),(QX, qy) = mnist.load_data()
 
+# Configuration parameters
+N = 5 # 1
+resize_dig = 14
+TREES = 64 # 4
+NEIGHBOURS = 256 # 1
 
-# Increase the size training set
+ax = np.empty((N*AX.shape[0], resize_dig, resize_dig))
 
-N = 5
-ax = np.empty((N*AX.shape[0], 14, 14))
-
+print()
+print("*** Size of dataset : {} ***".format(N*AX.shape[0]))
 # original
 for i in range( AX.shape[0] ):
     # AX_inc[i] = AX[i]
-    ax[i] = cv2.resize( AX[i % AX.shape[0]], (14, 14) )
+    ax[i] = cv2.resize( AX[i % AX.shape[0]], (resize_dig, resize_dig) )
 
-# north
-for i in range( AX.shape[0], 2*AX.shape[0] ):  
-    tmp = cv2.resize( AX[i - AX.shape[0]], (14,14) )
-    a = np.concatenate((tmp[1:,:], np.array([tmp[0,:]])), axis=0)
-    ax[i] = a
+# Increase the size training set
+print("*> Increasing the size of data set ...")
+if N > 1:
+    # north
+    for i in range( AX.shape[0], 2*AX.shape[0] ):  
+        tmp = cv2.resize( AX[i - AX.shape[0]], (resize_dig,resize_dig) )
+        a = np.concatenate((tmp[1:,:], np.array([tmp[0,:]])), axis=0)
+        ax[i] = a
 
-# south
-for i in range( 2*AX.shape[0], 3*AX.shape[0] ):  
-    tmp = cv2.resize( AX[i - 2*AX.shape[0]], (14,14) )
-    # print(tmp[-1,:])
-    # print(np.array([tmp[:-1,:]]))
-    a = np.concatenate((np.array([tmp[-1,:]]), tmp[:-1,:]), axis=0)
-    ax[i] = a
+    # south
+    for i in range( 2*AX.shape[0], 3*AX.shape[0] ):  
+        tmp = cv2.resize( AX[i - 2*AX.shape[0]], (resize_dig,resize_dig) )
+        # print(tmp[-1,:])
+        # print(np.array([tmp[:-1,:]]))
+        a = np.concatenate((np.array([tmp[-1,:]]), tmp[:-1,:]), axis=0)
+        ax[i] = a
 
-# east
-for i in range( 3*AX.shape[0], 4*AX.shape[0] ):  
-    tmp = cv2.resize( AX[i - 3*AX.shape[0]], (14,14) )
-    # print(tmp[:,1:])
-    # print(np.array([tmp[:,0]]))
-    a = np.concatenate((tmp[:,1:], np.array([tmp[:,0]]).reshape((14,1))), axis=1)
-    ax[i] = a
+    # east
+    for i in range( 3*AX.shape[0], 4*AX.shape[0] ):  
+        tmp = cv2.resize( AX[i - 3*AX.shape[0]], (resize_dig,resize_dig) )
+        # print(tmp[:,1:])
+        # print(np.array([tmp[:,0]]))
+        a = np.concatenate((tmp[:,1:], np.array([tmp[:,0]]).reshape((resize_dig,1))), axis=1)
+        ax[i] = a
 
-# west
-for i in range( 3*AX.shape[0], 4*AX.shape[0] ):  
-    tmp = cv2.resize( AX[i - 3*AX.shape[0]], (14, 14) )
-    # print(tmp[:,1:])
-    # print(np.array([tmp[:,0]]))
-    a = np.concatenate((np.array([tmp[:,-1]]).reshape((14,1)), tmp[:,:-1] ), axis=1)
-    ax[i] = a
-
-
-
+    # west
+    for i in range( 4*AX.shape[0], 5*AX.shape[0] ):  
+        tmp = cv2.resize( AX[i - 4*AX.shape[0]], (resize_dig, resize_dig) )
+        # print(tmp[:,1:])
+        # print(np.array([tmp[:,0]]))
+        a = np.concatenate((np.array([tmp[:,-1]]).reshape((resize_dig,1)), tmp[:,:-1] ), axis=1)
+        
+        ax[i] = a
 
 
 # Test data
-qx = np.empty( (QX.shape[0], 14, 14) )
+qx = np.empty( (QX.shape[0], resize_dig, resize_dig) )
 
 for i in range( QX.shape[0] ):  
-    qx[i] = cv2.resize( QX[i], (14, 14) )
+    qx[i] = cv2.resize( QX[i], (resize_dig, resize_dig) )
 
 ax = ax.astype('float32')
 qx = qx.astype('float32')
@@ -69,15 +74,18 @@ ax /= 255.0 #0 a 1
 qx /= 255.0 #0 a 1
 qp = np.empty(qy.shape, dtype="uint8")
 
+
 # Train KDtree
+print("*> Trainig the KDTREE ...")
 t1 = time.time()
 FLANN_INDEX_KDTREE = 1;# bug: flann enums are missing
-flann_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 64) 
+flann_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = TREES) 
 flann = cv2.flann_Index(ax, flann_params)
+
 
 # Search our KDtree with 256 neighbors
 t2 = time.time()
-matches, dists = flann.knnSearch(qx, 256)
+matches, dists = flann.knnSearch(qx, NEIGHBOURS)
 t3 = time.time()
 
 
@@ -93,9 +101,10 @@ for l in range( matches.shape[0] ):
         erros += 1
 
 # Display error rate and times
-print("Erros=%5.2f%%"%(100.0*erros/qy.shape[0]))
-print("Tempo de treinamento: %f"%(t2-t1))
-print("Tempo de predicao: %f"%(t3-t2))
+print("*>> Erros=%5.2f%%"%(100.0*erros/qy.shape[0]))
+print("*>> Tempo de treinamento: %f sec."%(t2-t1))
+print("*>> Tempo de predicao: %f sec."%(t3-t2))
+
 
 # # Listing 2.6 Displaying the fourth digit
 # import matplotlib.pyplot as plt
@@ -103,8 +112,8 @@ print("Tempo de predicao: %f"%(t3-t2))
 # # digit = AX[20, :, :]
 # # digit = digit.reshape((28,28))
 
-# digit = ax[3*AX.shape[0] - 20, :, :]
-# digit = digit.reshape((14,14))
+# digit = ax[5*AX.shape[0] - 200, :, :]
+# # digit = digit.reshape((resize_dig,resize_dig))
 
 
 # plt.imshow(digit, cmap = plt.cm.binary)
